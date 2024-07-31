3268fe830b45b75913bed3a8a7c790be6d046506
PascalGitz <pascal.gitz@hotmail.ch>
2024-07-31 11:24:49 +0200

    test of a markdown description

--- engicalc/concrete.py
+++ engicalc/concrete.py
@@ -1,2 +1,17 @@
-def kriechdehnung_SIA262_12(psi, epsilon_c_el):
+from IPython.display import Markdown, display 
+
+def kriechdehnung_SIA262_12(psi, epsilon_c_el, background=False):
+    """
+    psi = Kriechzahl
+    epsilon_c_el = elastische Dehnung
+
+    """
+    if background == True:
+        display(Markdown("""
+*SIA 262:2013 Ziff 3.1.2.6.1*
+
+Die Dehnung infolge Kriechens des Betons wird aus den elastischen Dehnungen mithilfe der Kriechzahl bestimmt:
+$$ \\varepsilon_{cc}(t) = \\varphi(t, t_0) \\varepsilon_{c,el} $$                         
+                         """))
+        
     return psi*epsilon_c_el
