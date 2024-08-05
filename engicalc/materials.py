9c85d2a6988cc8eafb68ba825a89d4243c876f99
Endorriar <marco.imesch@hotmail.com>
2024-08-05 20:20:25 +0200

    added other concrete NPK's

--- engicalc/materials.py
+++ engicalc/materials.py
@@ -27,17 +27,83 @@ def Betonsorte(NPK:str):
     """
 
     if NPK == "A":
-        print("NPK A wurde noch nicht definiert")
+        NPK_A = {
+            "Name": ("NPK A",),
+            "Druckfestigkeitsklasse": ("C20/25",),
+            "Expositionsklassen": ("XC2",),
+            "Nennwert Grösstkorn D_max": 32*mm,
+            "Klasse des Chloridgehalts Cl": 0.10,
+            "Konsistenzklasse": ("C3",),
+            "Frost-Tausalz-Widerstand": None,
+            "f_ck": 20*N/mm**2,
+            "f_ctm": 2.2*N/mm**2
+        }
+        return NPK_A
     elif NPK == "B":
-        print("NPK B wurde noch nicht definiert")
+        NPK_B = {
+            "Name": ("NPK B",),
+            "Druckfestigkeitsklasse": ("C25/30",),
+            "Expositionsklassen": ("XC3",),
+            "Nennwert Grösstkorn D_max": 32*mm,
+            "Klasse des Chloridgehalts Cl": 0.10,
+            "Konsistenzklasse": ("C3",),
+            "Frost-Tausalz-Widerstand": None,
+            "f_ck": 25*N/mm**2,
+            "f_ctm": 2.6*N/mm**2
+        }
+        return NPK_B
     elif NPK == "C":
-        print("NPK C wurde noch nicht definiert")
+        NPK_C = {
+            "Name": ("NPK C",),
+            "Druckfestigkeitsklasse": ("C30/37",),
+            "Expositionsklassen": ("XC4", "XF1"),
+            "Nennwert Grösstkorn D_max": 32*mm,
+            "Klasse des Chloridgehalts Cl": 0.10,
+            "Konsistenzklasse": ("C3",),
+            "Frost-Tausalz-Widerstand": None,
+            "f_ck": 30*N/mm**2,
+            "f_ctm": 2.9*N/mm**2
+        }
+        return NPK_C
     elif NPK == "D":
-        print("NPK D wurde noch nicht definiert")
+        NPK_D = {
+            "Name": ("NPK D",),
+            "Druckfestigkeitsklasse": ("C25/30",),
+            "Expositionsklassen": ("XC4", "XD1", "XF2"),
+            "Nennwert Grösstkorn D_max": 32*mm,
+            "Klasse des Chloridgehalts Cl": 0.10,
+            "Konsistenzklasse": ("C3",),
+            "Frost-Tausalz-Widerstand": "mittel",
+            "f_ck": 25*N/mm**2,
+            "f_ctm": 2.6*N/mm**2
+        }
+        return NPK_D
     elif NPK == "E":
-        print("NPK E wurde noch nicht definiert")
+        NPK_E = {
+            "Name": ("NPK E",),
+            "Druckfestigkeitsklasse": ("C25/30",),
+            "Expositionsklassen": ("XC4", "XD1", "XF4"),
+            "Nennwert Grösstkorn D_max": 32*mm,
+            "Klasse des Chloridgehalts Cl": 0.10,
+            "Konsistenzklasse": ("C3",),
+            "Frost-Tausalz-Widerstand": "hoch",
+            "f_ck": 25*N/mm**2,
+            "f_ctm": 2.6*N/mm**2
+        }
+        return NPK_E
     elif NPK == "F":
-        print("NPK F wurde noch nicht definiert")
+        NPK_F = {
+            "Name": ("NPK F",),
+            "Druckfestigkeitsklasse": ("C30/37",),
+            "Expositionsklassen": ("XC4", "XD3", "XF2"),
+            "Nennwert Grösstkorn D_max": 32*mm,
+            "Klasse des Chloridgehalts Cl": 0.10,
+            "Konsistenzklasse": ("C3",),
+            "Frost-Tausalz-Widerstand": "mittel",
+            "f_ck": 30*N/mm**2,
+            "f_ctm": 2.9*N/mm**2
+        }
+        return NPK_F
     elif NPK == "G":
         NPK_G = {
             "Name": ("NPK G",),
@@ -52,12 +118,56 @@ def Betonsorte(NPK:str):
         }
         return NPK_G
     elif NPK == "H":
-        print("NPK H wurde noch nicht definiert")
+        NPK_H = {
+            "Name": ("NPK H",),
+            "Druckfestigkeitsklasse": ("C25/30",),
+            "Expositionsklassen": None,
+            "Nennwert Grösstkorn D_max": 32*mm,
+            "Klasse des Chloridgehalts Cl": 0.10,
+            "Konsistenzklasse": ("F4",),
+            "Frost-Tausalz-Widerstand": "kein oder mittel",
+            "f_ck": 25*N/mm**2,
+            "f_ctm": 2.6*N/mm**2
+        }
+        return NPK_H
     elif NPK == "I":
-        print("NPK I wurde noch nicht definiert")
+        NPK_I = {
+            "Name": ("NPK I",),
+            "Druckfestigkeitsklasse": ("C25/30",),
+            "Expositionsklassen": None,
+            "Nennwert Grösstkorn D_max": 32*mm,
+            "Klasse des Chloridgehalts Cl": 0.10,
+            "Konsistenzklasse": ("F5",),
+            "Frost-Tausalz-Widerstand": "kein oder mittel",
+            "f_ck": 25*N/mm**2,
+            "f_ctm": 2.6*N/mm**2
+        }
+        return NPK_I
     elif NPK == "K":
-        print("NPK K wurde noch nicht definiert")
+        NPK_K = {
+            "Name": ("NPK K",),
+            "Druckfestigkeitsklasse": ("C20/25",),
+            "Expositionsklassen": None,
+            "Nennwert Grösstkorn D_max": 32*mm,
+            "Klasse des Chloridgehalts Cl": 0.10,
+            "Konsistenzklasse": ("F4",),
+            "Frost-Tausalz-Widerstand": None,
+            "f_ck": 20*N/mm**2,
+            "f_ctm": 2.2*N/mm**2
+        }
+        return NPK_K
     elif NPK == "L":
-        print("NPK L wurde noch nicht definiert")
+        NPK_L = {
+            "Name": ("NPK L",),
+            "Druckfestigkeitsklasse": ("C20/25",),
+            "Expositionsklassen": None,
+            "Nennwert Grösstkorn D_max": 32*mm,
+            "Klasse des Chloridgehalts Cl": 0.10,
+            "Konsistenzklasse": ("F5",),
+            "Frost-Tausalz-Widerstand": None,
+            "f_ck": 20*N/mm**2,
+            "f_ctm": 2.2*N/mm**2
+        }
+        return NPK_L
     else:
         raise ValueError('Betonsorte nicht erkannt. Inputmöglichkeiten: "A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L"')
