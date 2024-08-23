7bb6a70ad065c1a6c14c1e68d96eec176bd6d397
Endorriar <marco.imesch@hotmail.com>
2024-08-23 16:56:49 +0200

    added function 10

--- engicalc/actions.py
+++ engicalc/actions.py
@@ -2,25 +2,21 @@ from IPython.display import Markdown, display
 from numpy import sqrt, pi, tan, arctan, cos, arccos, sin, arcsin
 from engicalc.units import ureg, kg, t, mm, cm, dm, m, km, N, kN, MN, rad, deg, percent, s, MPa, los
 
-def Schneelast_charakteristisch_SIA261_9(mu_i:float, C_e:=1.0, C_T:float, s_k:float, background=False):
+def schneelast_charakteristisch_SIA261_9(mu_i:float, C_e:float, C_T:float, s_k:float, background=False):
     """
     Berechnung der charakteristischen Schneelast gemäss SIA 262:2013, Abschnitt 5.2.2\\
-    Diese Funktion berechnet die charakteristische Schneelast basierend auf der der Dachformbeiwerte, Windexposition und dem thermischen Beiwert.
+    Diese Funktion berechnet die charakteristische Schneelast basierend auf der Dachformbeiwerte, Windexposition und dem thermischen Beiwert.
     
     Parameter:
     ----------
     mu_i : float; 
-        Die Kriechzahl.
+        Der Dachformbeiwert.
         
     C_e : float; 
         Die Windexposition des Bauwerks. 
-        Gemäss 5.2.4: 
-        normale Exposition: 1.0
-        dem Wind stark ausgesetzte Lage: 0.8
-        vor Wind geschützter Lage: 1.2
 
     C_T : float; 
-        Die thermische Beiwert.
+        Der thermische Beiwert.
 
     s_k : float; 
         Schneelast auf horizontalem Gelände.
@@ -31,20 +27,29 @@ def Schneelast_charakteristisch_SIA261_9(mu_i:float, C_e:=1.0, C_T:float, s_k:fl
     Rückgabe:
     -------
     float
-        Die Kriechdehnung des Betons.
+        Die charakteristische Schneelast.
     
     Hinweise:
     -----
-    Gemäss SIA 261:2014, Abschnitt 5.2.2, wird die charakteristische Schneelast basierend auf der der Dachformbeiwerte, Windexposition und dem thermischen Beiwert bestimmt:
+    5.2.4\\
+    Je nach Windexposition des Bauwerks gilt für den Expositionsbeiwert Ce:\\
+    normale Windexposition Ce = 1,0\\
+    dem Wind stark ausgesetzte Lage Ce = 0,8\\
+    vor Wind geschützte Lage Ce = 1,2\\
+    5.2.5\\
+    Der thermische Beiwert CT ist im Allgemeinen gleich 1,0 zu setzen. Die Annahme eines kleineren Beiwerts
+    ist möglich, wenn ein grosser Wärmedurchgang an der Dachoberfläche den Schmelzprozess beschleunigt. 
+    Der Einfluss des Ausfalls technischer Installationen auf den angenommenen Wärmedurchgang ist dabei zu 
+    prüfen, beispielsweise bei Giasdächern über geheizten Räumen. 
   
 
     Beispiele:
     --------
-    >>> kriechdehnung_SIA262_12(1.5, 0.002)
-    0.003
+    >>> schneelast_charakteristisch_SIA261_9(1.78*kN/m**2, 1.0, 1.0, 1.5)
+    2.67*kN/m**2
     
-    >>> kriechdehnung_SIA262_12(2.0, 0.0025)
-    0.005
+    >>> schneelast_charakteristisch_SIA261_9(0.9*kN/m**2, 1.0, 1.0, 0.8)
+    0.72*kN/m**2
     """
 
     if background == True:
@@ -56,3 +61,48 @@ $$ q_k = \\mu_i \\cdot C_e \\cdot C_T \\cdot s_k $$
         """))
         
     return mu_i * C_e * C_T * s_k
+
+
+def schneelast_horizontal_charakteristisch_SIA261_10(h_0:float, background=False):
+    """
+    Berechnung der charakteristischen Schneelast auf horizontalem Gelände gemäss SIA 262:2013, Abschnitt 5.2.6\\
+    Diese Funktion berechnet die charakteristische Schneelast basierend auf der Bezugshöhe h_0 in m.
+    
+    Parameter:
+    ----------
+    h_0 : float; 
+        Die Bezugshöhe. Eingabe in m **dimensionslos!**
+        
+    background : bool, optional; 
+        Wenn True, wird die entsprechende Formel aus SIA 262:2013 angezeigt. Standard ist False.
+    
+    Rückgabe:
+    -------
+    float
+        Die charakteristische Schneelast im horizontalen Gelände.
+    
+    Hinweise:
+    -----
+    Gemäss SIA 261:2014, Abschnitt 5.2.6, wird die charakteristische Schneelast im horizontalen Gelände bestimmt:
+  
+
+    Beispiele:
+    --------
+    >>> schneelast_horizontal_charakteristisch_SIA261_10(650)
+    1.78 *kN/m**2
+    
+    >>> schneelast_horizontal_charakteristisch_SIA261_10(50)
+    0.9 *kN/m**2
+    """
+
+    if background == True:
+        display(Markdown("""
+*SIA 261:2014 Abschnitt 5.2.6 (10)*
+
+Der charakteristische Wert der Schneelast im horizontalen Gelände beträgt:
+$$ s_k = \\left[1 + \\left(\\frac{h_0}{350}\\right)^2\\right] \\cdot 0{,}4 \\, \\text{kN/m}^2 \\geq 0{,}9 \\, \\text{kN/m}^2 $$
+        """))
+    
+
+
+    return max((1 + (h_0 / 350)**2) * 0.4 *kN/m**2, 0.9 *kN/m**2)
