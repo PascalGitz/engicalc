46a3b2d9936ecbaea1aa70dbc775c84b81c85c0f
PascalGitz <pascal.gitz@hotmail.ch>
2024-08-26 09:24:19 +0200

    version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='engicalc',
-    version='0.1.0',
+    version='v0.0.1',
     packages=find_packages(),
     install_requires=[
         # List your package dependencies here
