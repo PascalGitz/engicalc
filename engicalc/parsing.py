d8382096e2dffefb7267a66af79b3c37b149b150
Pascal Gitz <p.gitz@bp-ing.ch>
2025-11-20 07:17:47 +0100

    added function intermediate

--- engicalc/parsing.py
+++ engicalc/parsing.py
@@ -60,6 +60,8 @@ def parse(code: str, show_name, show_expression, show_value, precision) -> list:
                 if (hasattr(func, 'id') and func.id == 'Cell') or (hasattr(func, 'attr') and func.attr == 'Cell'):
                     continue
             results.append(Assignment(get_code(node, lines), show_name=show_name, show_expression=show_expression, show_value=show_value, precision=precision))
+        if isinstance(node, ast.FunctionDef):
+            
         elif isinstance(node, ast.Expr) and isinstance(node.value, ast.Name):
             # Single variable name as a statement (e.g., 'var')
             results.append(Name(node.value.id, show_name=show_name, show_expression=show_expression, show_value=show_value, precision=precision))
